ff191b30a61c819da677f202bef7eb26bdddb0a9
Jinhui Weng <jasonweng95@gmail.com>
2023-03-19 17:15:10 +1100

    testing

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,7 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
-  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+  mongoose
+    .connect(config.get("db"), { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => winston.info(`Connected to ${config.get("db")}...`));
 };

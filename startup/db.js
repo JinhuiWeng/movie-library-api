1fa29d435e227ea8840293c5c076437ef4f2b3c1
Jinhui Weng <jasonweng95@gmail.com>
2023-03-19 23:46:37 +1100

    adding env for test

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,12 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
-  console.log(db);
+
   mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => winston.info(`Connected to ${db}...`));
+    .connect(process.env.MONGODB_URL)
+    .then(() => winston.info(`Connected to Mongodb`));
+
+  // mongoose
+  //   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  //   .then(() => winston.info(`Connected to ${db}...`));
 };
